7105e964182a068d2184061a91cb9c0e21f64e28
AzureDescent <drownfish723@sjtu.edu.cn>
2025-11-06 21:46:09 +0800

    feat: 增加USART的代码框架

--- UserCode/Src/RTOS/callback.cpp
+++ UserCode/Src/RTOS/callback.cpp
@@ -4,7 +4,22 @@
 #include "can.h"
 #include "tim.h"
 #include "cmsis_os2.h"
-// FIXME: "cmsis_os2.h" is temporary for osMessageQueuePut, which may be removed after integrating with other head files
+#include "usart.h"
+#include "string.h"
+#include "dma.h"
+// FIXME: resolve the inclusion relationship of header files after the project is stable
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+    if (huart == &huart3)
+    {
+        // TODO: Receive data processing in Class Remote Control
+        // URL: https://github.com/AzureDescent/C-Type_Board/blob/RemoteControl/Core/Src/callback.cpp
+
+        // TODO: Define rcDataReadySemaphoreHandle appropriately in RTOS setup
+        osSemaphoreRelease(rcDataReadySemaphoreHandle);
+    }
+}
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
 {
@@ -16,7 +31,9 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data) == HAL_OK)
         {
             uint8_t queue_message[sizeof(CAN_RxHeaderTypeDef) + 8];
-            // TODO: Process queue_message as needed
+            // TODO: Process queue_message better as needed
+            memcpy(queue_message, &rx_header, sizeof(CAN_RxHeaderTypeDef));
+            memcpy(queue_message + sizeof(CAN_RxHeaderTypeDef), rx_data, 8);
 
             // TODO: Define canRxQueueHandle appropriately in RTOS setup
             osMessageQueuePut(canRxQueueHandle, &queue_message, 0, 0);
