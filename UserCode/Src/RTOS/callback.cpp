220b981d654daa5946d4b0fcb1c8c1e3288f9fe9
AzureDescent <drownfish723@sjtu.edu.cn>
2025-11-06 21:18:18 +0800

    feat: 增加CAN通信的代码框架

--- UserCode/Src/RTOS/callback.cpp
+++ UserCode/Src/RTOS/callback.cpp
@@ -3,6 +3,26 @@
 //
 #include "can.h"
 #include "tim.h"
+#include "cmsis_os2.h"
+// FIXME: "cmsis_os2.h" is temporary for osMessageQueuePut, which may be removed after integrating with other head files
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
+{
+    CAN_RxHeaderTypeDef rx_header;
+    uint8_t rx_data[8];
+
+    if (hcan -> Instance == CAN1)
+    {
+        if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data) == HAL_OK)
+        {
+            uint8_t queue_message[sizeof(CAN_RxHeaderTypeDef) + 8];
+            // TODO: Process queue_message as needed
+
+            // TODO: Define canRxQueueHandle appropriately in RTOS setup
+            osMessageQueuePut(canRxQueueHandle, &queue_message, 0, 0);
+        }
+    }
+}
 
 uint32_t count = 0;
 
