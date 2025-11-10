6d0795680b6a8075f898ec3a4d4c40488f948d3a
AzureDescent <drownfish723@sjtu.edu.cn>
2025-11-10 13:28:30 +0800

    refactor: 移除rtos.h和rtos.cpp中与IMU无关的代码

--- UserCode/Inc/RTOS/rtos.h
+++ UserCode/Inc/RTOS/rtos.h
@@ -8,31 +8,12 @@
 #include "Gimbal.h"
 
 // TODO: Add and FIX external declarations
-extern Gimbal gimbal_controller;
 extern IMU imu_sensor;
-extern RemoteControl rc_controller;
 
 // TODO: Add and FIX task handle declarations
-extern osThreadId_t control_task_handle;
 extern osThreadId_t imu_task_handle;
-extern osThreadId_t can_send_task_handle;
-extern osThreadId_t can_recv_task_handle;
-extern osThreadId_t rc_process_task_handle;
-extern osThreadId_t iwdg_task_handle;
 
-extern osThreadId_t can_rx_queue_handle;
-
-extern osSemaphoreId_t rc_data_ready_semaphore_handle;
-extern osSemaphoreId_t imu_data_ready_semaphore_handle;
-
-extern osMutexId_t gimbal_mutex_handle;
-
-void VControlTask(void *argument);
 [[noreturn]] void VImuTask(void *argument);
-[[noreturn]] void VCanSendTask(void *argument);
-[[noreturn]] void VCanRecvTask(void *argument);
-[[noreturn]] void VRcProcessTask(void *argument);
-[[noreturn]] void VIwdgTask(void *argument);
 
 
 #endif //FINALTASK_RTOS_H
